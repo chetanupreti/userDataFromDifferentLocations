6f4a01b2ef74b3dd355bbfcb61946e2ce72afde1
chetanupreti <sidharthaupreti@gmail.com>
2019-07-23 22:03:38 +0530

    routes and functions define

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var userData = require('./get-user-data/user-data')
+const server = express();
+server.use(bodyParser.json())
+const port = process.env.PORT || 5000;
+server.use(cors());
+
+
+server.post('/getUsersData', (req, res) => {
+    {
+        let users;
+        users = req.body;
+        userData.getAllUserDetails(users);
+    }
+    res.send({yo:"yo"});
+});
+
+server.get('/', (req,res) => {
+    res.send({hye:'hye'})
+})
+
+server.listen(port, function () {
+    console.log(`server is started in ${port}`)
+});
