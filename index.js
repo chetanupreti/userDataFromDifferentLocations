d1aa2704bcc39f2bb85cdfa8ad801aa5ac76b0a4
chetan <sidharthaupreti@.com>
2019-07-29 11:04:43 +0530

    cors erroe

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 var userData = require('./get-user-data/user-data')
 const server = express();
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 var returnUserData;
 
